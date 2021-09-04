f197c3e3e7e50733dd7f450b1b2b432495988c38
wsxokm-XL <84434556+wsxokm-XL@users.noreply.github.com>
2021-09-04 18:45:42 +0800

    update

--- graces_alignment.cpp
+++ graces_alignment.cpp
@@ -1,14 +1,20 @@
+//graces_alignment.cpp
+/*
+将输入的学生成绩（学生个数<=1500,分数为整数且>=0 && <=150）进行排序，O(n)算法
+ps：若分数不为整数或者为负数，*10 或者 +固定值 使之为正整数
+*/
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
     struct node {
-        char name[5];
+        char *name;
     };
 
     struct stu {
-        struct node s[150];
+        struct node s[1500];
         int size=0;
     } num[151];
 
@@ -18,6 +24,7 @@ int main()
     for(int i=0;i<n;i++)
     {
         cin>>score;
+        num[score].s[num[score].size].name=new char[10];
         cin>>num[score].s[num[score].size].name;
         num[score].size++;
     }
@@ -29,6 +36,7 @@ int main()
         {
             m--;
             cout<<i<<" "<<num[i].s[m].name<<endl;
+            //delete num[i].s[m].name; //释放内存空间
         }
     }
 
